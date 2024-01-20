9936038951b9c7c41fbf3224b4c698c8afb72d4c
Roger <z5477692@ad.unsw.edu.au>
2024-01-20 12:10:46 +1100

    added links and side bar navigation:

--- website.js
+++ website.js
@@ -1,1 +1,13 @@
-// hello
+document.querySelectorAll(".sidenav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - 100,
+      behavior: "smooth",
+    });
+  });
+});
