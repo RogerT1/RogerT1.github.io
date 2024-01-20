2eb31d40dc34947e0514e04bf93f90e3cc910af3
Roger <z5477692@ad.unsw.edu.au>
2024-01-20 15:11:05 +1100

    lots of style changes

--- website.js
+++ website.js
@@ -2,12 +2,21 @@ document.querySelectorAll(".sidenav a").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const targetId = this.getAttribute("href").substring(1);
+    const targetId = this.getAttribute("href").substring(1); 
+    // this gets the tag without the #
     const targetElement = document.getElementById(targetId);
 
-    window.scrollTo({
-      top: targetElement.offsetTop - 100,
-      behavior: "smooth",
-    });
+    if (targetId === 'about') {
+        window.scrollTo({
+          top: targetElement.offsetTop - 300,
+          behavior: "smooth",
+        });
+    } else {
+        window.scrollTo({
+          top: targetElement.offsetTop - 100,
+          behavior: "smooth",
+        });
+    }
   });
 });
+
