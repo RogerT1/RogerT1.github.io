b3f444e974586807a38046c81d75aca44363c441
Roger <z5477692@ad.unsw.edu.au>
2024-01-20 19:14:22 +1100

    perfected the manga style

--- website.js
+++ website.js
@@ -2,21 +2,39 @@ document.querySelectorAll(".sidenav a").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    const targetId = this.getAttribute("href").substring(1); 
+    const targetId = this.getAttribute("href").substring(1);
     // this gets the tag without the #
     const targetElement = document.getElementById(targetId);
 
-    if (targetId === 'about') {
-        window.scrollTo({
-          top: targetElement.offsetTop - 1000,
-          behavior: "smooth",
-        });
+    if (targetId === "about") {
+      window.scrollTo({
+        top: targetElement.offsetTop - 1000,
+        behavior: "smooth",
+      });
     } else {
-        window.scrollTo({
-          top: targetElement.offsetTop - 100,
-          behavior: "smooth",
-        });
+      window.scrollTo({
+        top: targetElement.offsetTop - 100,
+        behavior: "smooth",
+      });
     }
   });
 });
 
+document.querySelectorAll(".about").forEach((anchor) => {
+  anchor.addEventListener("mouseenter", function (e) {
+    e.preventDefault();
+
+    hiddenText1 = document.getElementById("aboutme");
+    hiddenText1.classList.toggle("show"); 
+    biggerBox = document.querySelector(".biggerbox");
+    if (biggerBox.style.borderBottom === "none") {
+        biggerBox.style.borderBottom = "2px solid #000";
+    } else {
+        biggerBox.style.borderBottom = "none";
+    }
+      
+    
+    // Toggle the display of the hidden text
+    
+  });
+});
