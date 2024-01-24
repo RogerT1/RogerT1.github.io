883c21de39d3eb6c92143af35023582cffbd9c51
Roger <z5477692@ad.unsw.edu.au>
2024-01-25 01:30:49 +1100

    added animations to projects, changed the filter ont he video, changed about me section for different media

--- website.js
+++ website.js
@@ -20,158 +20,6 @@ document.querySelectorAll(".sidenav a").forEach((anchor) => {
   });
 });
 
-// document.querySelectorAll(".projectText1 a").forEach((anchor) => {
-//   anchor.addEventListener("click", function (e) {
-//     e.preventDefault();
-
-//     const targetId = this.getAttribute("href").substring(1);
-//     // this gets the tag without the #
-//     const targetElement = document.getElementById(targetId);
-//     const scrollContainer = document.querySelector(".scroll-container");
-//     if (targetId === "about") {
-//       scrollContainer.scrollTo({
-//         top: targetElement.offsetTop - 1000,
-//         behavior: "smooth",
-//       });
-//     } else {
-//       scrollContainer.scrollTo({
-//         top: targetElement.offsetTop - 50,
-//         behavior: "smooth",
-//       });
-//     }
-//   });
-// });
-
-
-function isElementVisible(element) {
-  if (!element) {
-    return false;
-  }
-
-  // Check if the element is hidden via display property
-  if (window.getComputedStyle(element).display === "none") {
-    return false;
-  }
-
-  var computedStyle = window.getComputedStyle(element);
-  if (computedStyle.opacity === "0") {
-    return false;
-  }
-
-  // Check if the element has dimensions
-  if (element.offsetWidth === 0 || element.offsetHeight === 0) {
-    return false;
-  }
-
-  // Check if the element is hidden by ancestor elements
-  if (!element.offsetParent) {
-    return false;
-  }
-
-  return true;
-}
-
-var buttonClicked = false;
-
-// document.getElementById("rewind").addEventListener("click", function () {
-//   buttonClicked = true;
-//   setTimeout(function () {
-//     buttonClicked = false;
-//   }, 2000);
-//   var hiddenText1 = document.getElementById("aboutme");
-
-//   var hiddenmin = document.getElementById("minimise1");
-//   if (isElementVisible(hiddenmin)) {
-//     hiddenmin.classList.toggle("show");
-//   }
-
-//   var hiddenText1 = document.getElementById("aboutme");
-//   hiddenText1.classList.toggle("show");
-
-//   var hiddenText2 = document.getElementById("aboutme1");
-
-//   setTimeout(function () {
-//     hiddenText2.classList.toggle("show");
-//   }, 500);
-
-//   var hiddenText5 = document.getElementById("aboutme2");
-
-//   setTimeout(function () {
-//     hiddenText5.classList.toggle("show");
-//   }, 1000);
-
-//   var hiddenText3 = document.getElementById("aboutme3");
-//   setTimeout(function () {
-//     hiddenText3.classList.toggle("show");
-//   }, 1500);
-
-//   var hiddenText4 = document.getElementById("aboutme4");
-//   setTimeout(function () {
-//     hiddenText4.classList.toggle("show");
-//   }, 2000);
-
-// });
-
-var isRunning = false;
-
-document.querySelectorAll(".about").forEach((anchor) => {
-  anchor.addEventListener("mouseenter", function (e) {
-    e.preventDefault();
-
-    if (isRunning) {
-      return; // Exit early if the code is already running
-    }
-
-    isRunning = true;
-
-    if (!buttonClicked) {
-      var hiddenmin = document.getElementById("minimise1");
-
-      if (!isElementVisible(hiddenmin)) {
-        setTimeout(function () {
-          hiddenmin.classList.toggle("show");
-        }, 2000);
-      }
-
-      var hiddenText1 = document.getElementById("aboutme");
-
-      if (!isElementVisible(hiddenText1)) {
-        hiddenText1.classList.toggle("show");
-      }
-
-      var hiddenText2 = document.getElementById("aboutme1");
-
-      if (!isElementVisible(hiddenText1)) {
-        setTimeout(function () {
-          hiddenText2.classList.toggle("show");
-        }, 500);
-      }
-
-      var hiddenText5 = document.getElementById("aboutme2");
-
-      if (!isElementVisible(hiddenText1)) {
-        setTimeout(function () {
-          hiddenText5.classList.toggle("show");
-        }, 1000);
-      }
-
-      var hiddenText3 = document.getElementById("aboutme3");
-      if (!isElementVisible(hiddenText1)) {
-        setTimeout(function () {
-          hiddenText3.classList.toggle("show");
-        }, 1500);
-      }
-      var hiddenText4 = document.getElementById("aboutme4");
-      if (!isElementVisible(hiddenText1)) {
-        setTimeout(function () {
-          hiddenText4.classList.toggle("show");
-        }, 2000);
-      }
-    }
-
-    isRunning = false;
-  });
-});
 document.addEventListener("DOMContentLoaded", function () {
   var aboutSlider1 = document.getElementById("aboutSlider1");
 
@@ -184,19 +32,15 @@ document.addEventListener("DOMContentLoaded", function () {
   aboutSlider2.style.opacity = "100%";
 });
 
+//////////////////////////////
+// No zooming
 document.addEventListener("touchmove", handleTouchMove, { passive: false });
 document.addEventListener("wheel", handleWheel, { passive: false });
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.key === "=") {
     event.preventDefault();
   }
-  
-
-});
-
-
-document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.key === "-") {
     event.preventDefault();
   }
@@ -214,4 +58,64 @@ function handleWheel(event) {
     event.preventDefault();
   }
 }
+//////////////////////////////
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".filter").forEach(function (videoHover) {
+    videoHover.addEventListener("mouseenter", function () {
+      var video = videoHover.querySelector(".video");
+      video.play();
+    });
+  });
+
+  document.querySelectorAll(".filter").forEach(function (videoHover) {
+    videoHover.addEventListener("mouseleave", function () {
+      var video = videoHover.querySelector(".video");
+      video.pause();
+    });
+  });
+});
+
+// Project Animation
+const scrollContainer = document.querySelector(".scroll-container");
+scrollContainer.addEventListener("scroll", function () {
+  var scrollY = scrollContainer.scrollTop;
+  var windowSize = window.innerWidth;
+  const targetElement = document.getElementById("projects");
+  const scrollActivationHeight1 = targetElement.offsetTop - 700;
+  const scrollActivationHeight2 = targetElement.offsetTop - 200;
+  const scrollActivationHeight3 = targetElement.offsetTop + 300;
+  const scrollActivationHeight4 = targetElement.offsetTop + 800;
+  var projOne = document.getElementById("oneOne");
+  var projTwo = document.getElementById("oneTwo");
+  var projThree = document.getElementById("twoOne");
+  var projFour = document.getElementById("twoTwo");
+  if (scrollY >= scrollActivationHeight1 && windowSize >= 700) {
+    projOne.style.left = 0;
+    projOne.style.opacity = "100%";
+    projTwo.style.right = 0;
+    projTwo.style.opacity = "100%";
+  }
+  if (scrollY >= scrollActivationHeight2 && windowSize >= 700) {
+    projThree.style.left = 0;
+    projThree.style.opacity = "100%";
+    projFour.style.right = 0;
+    projFour.style.opacity = "100%";
+  }
+  if (scrollY >= scrollActivationHeight1 && windowSize < 700) {
+    projOne.style.left = 0;
+    projOne.style.opacity = "100%";
+  }
+  if (scrollY >= scrollActivationHeight2 && windowSize < 700) {
+    projTwo.style.right = 0;
+    projTwo.style.opacity = "100%";
+  }
+  if (scrollY >= scrollActivationHeight3 && windowSize < 700) {
+    projThree.style.left = 0;
+    projThree.style.opacity = "100%";
+  }
+  if (scrollY >= scrollActivationHeight4 && windowSize < 700) {
+    projFour.style.right = 0;
+    projFour.style.opacity = "100%";
+  }
+});
